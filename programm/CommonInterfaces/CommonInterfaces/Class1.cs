2d97d56888ee681f2863fbc274011a0780034eb2
JoshiKeks <paulschult@yahoo.de>
2020-11-17 16:50:12 +0100

    Change Request eingeführt, Projektsetups erstellt, CommonInterface erstellt

---
+++ programm/CommonInterfaces/CommonInterfaces/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterfaces
+{
+    public interface IMLAdapter
+    {
+        void trainModel(DataTable trainingData);
+        void testModel(DataTable testData);
+        void loadModel(string filepath);
+        void saveModel(string filepath);
+        DataTable predictAndReturnResults(DataTable rawData);
+
+
+    }
+
+
+
+    public interface IDataManager
+    {
+
+    }
+
+    public interface IStatistics
+    {
+
+    }
+}
