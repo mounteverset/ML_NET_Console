3fbb4ed864c9e0106e9e223feaf194d4f83f3091
Lukas Evers <s0570976@htw-berlin.de>
2020-11-18 21:49:46 +0100

    Renamed MLAdapter class1.cs to MLAdapter.cs; implemented the interface; in the Dummy GUI every public Method is called and made sure they are visible

--- programm/MLAdapter/DummyGUI_MLAdapter/Program.cs
+++ programm/MLAdapter/DummyGUI_MLAdapter/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using MLAdapter;
+
 
 namespace DummyGUI_MLAdapter
 {
@@ -10,6 +13,17 @@ namespace DummyGUI_MLAdapter
     {
         static void Main(string[] args)
         {
+            
+            MLAdapter.MLAdadpter mLAdapter = new MLAdadpter();
+            mLAdapter.trainModel(new DataTable());
+            mLAdapter.testModel(new DataTable());
+            DataTable results = new DataTable();
+            mLAdapter.predictAndReturnResults(results);
+            var resultView = results.DefaultView;
+            
+            mLAdapter.saveModel("hier");
+            mLAdapter.loadModel("dort");
+
         }
     }
 }
---
+++ programm/MLAdapter/MLAdapter/MLAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInterfaces;
+using System.Data;
+using Microsoft.ML;
+
+
+namespace MLAdapter
+{
+    public class MLAdadpter : IMLAdapter
+    {
+        #region Attributes
+        private MLContext MLContext { get; set; }
+        private ITransformer MLModel { get; set; }
+        private string ModelFilePath { get; set; }
+        private IDataView TrainingData { get; set; }
+        private IDataView TestData { get; set; }
+        private IDataView UserData { get; set; }
+        private PredictionEngine<object,object> PredictionEngine { get; set; }
+
+
+        #endregion
+
+        #region Methods
+        public void trainModel(DataTable trainingData)
+        {
+            throw new NotImplementedException();
+        }
+        public void testModel(DataTable testData)
+        {
+            throw new NotImplementedException();
+
+        }
+        public void loadModel(string filepath)
+        {
+            throw new NotImplementedException();
+
+        }
+        public void saveModel(string filepath)
+        {
+            throw new NotImplementedException();
+        }
+        public DataTable predictAndReturnResults(DataTable rawData)
+        {
+
+            return new DataTable();
+
+        }
+        #endregion
+
+        #region Constructors
+        public MLAdadpter() { }
+
+        #endregion
+    }
+}
