4fed7adddf5a489c34315a3ec14dad9ab640d04a
JoshiKeks <paulschult@yahoo.de>
2020-11-18 23:41:17 +0100

    Change request zu Commoninterfaces gestellt, erste Implementierungen der Dummykomponenten, Statistics Modul als  Bibliothek erstellt, Dateinamen angepasst

---
+++ programm/GUIandStatistics/DummyDataManager/DummyDataManager/DummyDataManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInterfaces;
+
+namespace DummyDataManager
+{
+    public class DummyDataManager : IDataManager
+    {
+        public DataTable UserTable => throw new NotImplementedException();
+
+        public int AmountOfColumns { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int AmountOfRows { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public DataTable ML_Result => throw new NotImplementedException();
+
+        public DataTable convertCSVtoDataTable()    // muss nicht Teil des Interfaces sein
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getAmountOfColumns()
+        {
+            return 4;   // schön Wären hier Coner-Cases und in Abstimmung mit den tatsächlichen Spalten der Datatable
+        }
+
+        public void loadCSV(string filepath)
+        {
+            // Exceptionhandling nötig im Dummy?
+        }
+
+        public void saveCSV(string filepath, string filename)
+        {
+            // Exceptionhandling nötig im Dummy?
+        }
+
+        public void setColumnsType(int[] dataColumns, int resultColumn)
+        {
+           // Exceptionhandling nötig im Dummy?
+        }
+
+        public void setMLResult(DataTable mlResult)
+        {
+            // Exceptionhandling nötig im Dummy?
+        }
+    }
+}
