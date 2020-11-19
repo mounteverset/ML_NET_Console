564a4e32ae472dc8133117353d6d7e54d0228fba
Lukas Evers <s0570976@htw-berlin.de>
2020-11-19 18:55:17 +0100

    Erstellung von Helferfunktionen, die die Erstellung von DataTables aus CSV Dateien realisieren, um damit das Machine Learning Modell zu trainieren

---
+++ programm/MLAdapter/DummyGUI_MLAdapter/HelperFunctions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DummyGUI_MLAdapter
+{
+    public class HelperFunctions
+    {
+        public static void PrintDataTableToConsole(DataTable dt)
+        {
+            for (int i = 0; i < dt.Rows.Count; i++) //looping through all rows including the column. change `i=1` if need to exclude the columns display
+            {
+                for (int j = 0; j < dt.Columns.Count; j++) //looping through all columns
+                {
+                    Console.Write(dt.Rows[i][j] + " "); //display of the data
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static DataTable ConvertCSVToDataTable(string filepath)
+        {
+            DataTable dt = new DataTable();
+            Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header);
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = CSVParser.Split(sr.ReadLine());
+                    DataRow dr = dt.NewRow();
+
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i].Replace("\"", string.Empty);
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            return dt;
+        }
+    }
+}
--- programm/MLAdapter/DummyGUI_MLAdapter/Program.cs
+++ programm/MLAdapter/DummyGUI_MLAdapter/Program.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using MLAdapter;
-
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DummyGUI_MLAdapter
 {
@@ -13,17 +14,21 @@ namespace DummyGUI_MLAdapter
     {
         static void Main(string[] args)
         {
-            
-            MLAdapter.MLAdadpter mLAdapter = new MLAdadpter();
-            mLAdapter.TrainModel(new DataTable());
-            mLAdapter.TestModel(new DataTable());
+            string filepath = @"../../../../Beispieldaten.iris.txt";
+            DataTable dt = HelperFunctions.ConvertCSVToDataTable(filepath);
+            HelperFunctions.PrintDataTableToConsole(dt);
+          
+            MLAdadpter mLAdapter = new MLAdadpter();           
+            mLAdapter.TrainModel(dt);
+            mLAdapter.TestModel(dt);
             DataTable data = new DataTable();
             List<int> results = mLAdapter.PredictAndReturnResults(data);
             
             
             mLAdapter.SaveModel("hier");
             mLAdapter.LoadModel("dort");
-
+            
         }
+       
     }
 }
