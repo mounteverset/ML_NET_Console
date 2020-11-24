759754198654a3b3c77b78523451b4c5e3efaeb0
JoshiKeks <paulschult@yahoo.de>
2020-11-24 18:37:09 +0100

    Änderungen im CommonInterface durchgeführ, siehe Change Request Einträge 24.11

--- programm/CommonInterfaces/CommonInterfaces/CommonInterfaces.cs
+++ programm/CommonInterfaces/CommonInterfaces/CommonInterfaces.cs
@@ -7,46 +7,146 @@ using System.Data;
 
 namespace CommonInterfaces
 {
-    public interface IMLAdapter<T>
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IMLAdapter
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="trainingData"></param>
         void TrainModel(DataTable trainingData);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="testData"></param>
+        /// <returns></returns>
         List<int> TestModel(DataTable testData);
         void LoadModel(string filepath);
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <param name="filename"></param>
         void SaveModel(string filepath, string filename);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rawData"></param>
+        /// <returns></returns>
         List<int> PredictAndReturnResults(DataTable rawData);
     }
 
 
-
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IDataManager
     {
         //readonly when instantiated
+        /// <summary>
+        /// 
+        /// </summary>
         DataTable UserTable { get; } 
 
 
         //readonly when instantiated
+        /// <summary>
+        /// 
+        /// </summary>
         List<int> ML_Result { get; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filepath"></param>
         void LoadCSV(string filepath);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <param name="filename"></param>
         void SaveCSV(string filepath, string filename);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         int GetAmountOfColumns();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dataColumns"></param>
+        /// <param name="resultColumn"></param>
         void SetColumnsType(int[] dataColumns, int resultColumn);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mlResult"></param>
         void SetMLResult(List<int> mlResult);
         
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IStatistics
     {
-        double calculateSensitivity(List<int> MLResult, List<int> realResult);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MLResult"></param>
+        /// <param name="realResult"></param>
+        /// <returns></returns>
+        double CalculateSensitivity(List<int> MLResult, List<int> realResult);
 
-        double calculateSpecificity(List<int> MLResult, List<int> realResult);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MLResult"></param>
+        /// <param name="realResult"></param>
+        /// <returns></returns>
+        double CalculateSpecificity(List<int> MLResult, List<int> realResult);
 
-        double calculatePrecision(List<int> MLResult, List<int> realResult);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MLResult"></param>
+        /// <param name="realResult"></param>
+        /// <returns></returns>
+        double CalculatePrecision(List<int> MLResult, List<int> realResult);
 
-        double calculateNegativePredictiveValue(List<int> MLResult, List<int> realResult);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MLResult"></param>
+        /// <param name="realResult"></param>
+        /// <returns></returns>
+        double CalculateNegativePredictiveValue(List<int> MLResult, List<int> realResult);
 
-        double calculatePositivePredictiveValue(List<int> MLResult, List<int> realResult);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MLResult"></param>
+        /// <param name="realResult"></param>
+        /// <returns></returns>
+        double CalculatePositivePredictiveValue(List<int> MLResult, List<int> realResult);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="MLResult"></param>
+        /// <param name="realResut"></param>
+        /// <returns></returns>
         Dictionary<string, int> CalculateConfusionMatrix(DataTable MLResult, DataTable realResut);
 
 
